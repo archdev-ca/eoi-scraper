6a181585cb90113818d0a010009e1906623f108a
Archie G <acgelonga@gmail.com>
2021-12-05 23:47:49 -0600

    Implement caching

--- eoiscraper.py
+++ eoiscraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from os.path import exists
 import requests
 import hashlib
+import re
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
@@ -12,7 +13,7 @@ with open("eoi.html", encoding="utf8") as fp:
 # r = requests.get("https://immigratemanitoba.com/category/mpnp-notices/eoi-draw/", headers=headers)
 # data = r.text
 
-soup = BeautifulSoup(data)
+soup = BeautifulSoup(data, features="html.parser")
 
 # Get all URLs
 nav = soup.find(id='archives')
@@ -25,13 +26,26 @@ for li in nav_lis:
 for link in urls:
 
   # Get all <article class="post">
-  htmlFilename = hashlib.md5(link)
+  htmlFilename = re.sub(r'[^a-zA-Z0-9]+', '-', link)
+  htmlFilename = re.sub(r'https-', '', htmlFilename)
+  htmlFilename = re.sub(r'-$', '', htmlFilename)
   htmlFile = Path('cache/' + htmlFilename)
-  htmlFile.touch(exist_ok=True)
-  f = open(myfile)
-  r  = requests.get(link, headers=headers)
-  data = r.text
-  print(data)
+  print("Checking cache: " + htmlFilename)
+
+  # Does cache file exist
+  if (exists(htmlFile)):
+    # Open cache File
+    data = open(htmlFile)
+  else :
+    # Open url
+    print("Opening URL: " + link)
+    data = requests.get(link, headers=headers)
+
+    # Save to cache
+    with open(htmlFile, 'w', encoding="utf-8") as f:
+      f.write(data.text)
+
+
 
     # Loop through each article
 
